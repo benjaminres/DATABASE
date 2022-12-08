9810ca631e2ca80f3c1d60a125aec603a6fb7450
benjaminres <120069618+benjaminres@users.noreply.github.com>
2022-12-07 21:11:57 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+print("@@@@@@@@@@@@@@@@@@@@")
+print("@@@@@          @@@@@")
+print("@@@@@           @@@@@")
+print("@@@@@            @@@@@")
+print("@@@@@              @@@@@")
+print("@@@@@               @@@@@")
+print("@@@@@                @@@@@")
+print("@@@@@                  @@@@@")
+print("@@@@@                    @@@@@")
+print("@@@@@                     @@@@@")
+print("@@@@@                      @@@@")
+print("@@@@@                       @@@@")
+print("@@@@@                        @@@@")
+print("@@@@@                        @@@@@")
+print("@@@@@                        @@@@@")
+print("@@@@@                       @@@@@")
+print("@@@@@                      @@@@@")
+print("@@@@@                     @@@@@")
+print("@@@@@                   @@@@@")
+print("@@@@@                 @@@@@")
+print("@@@@@                @@@@@")
+print("@@@@@              @@@@@")
+print("@@@@@            @@@@@")
+print("@@@@@          @@@@@")
+print("@@@@@        @@@@@")
+print("@@@@@@@@@@@@@@@@@")
+
+print("DataBase creator Benjamin Zaghloul")
+print("Contact me here 261198 ")
+print("Please note that I am not responsible for any destruction done using this tool PLEASE USE IT FOR ETHICAL PURPOSES ONLY!!! By using this tool you agree to the conditions stated.")
+#Temp data base for storage
+password = "2cr56zgj"
+LIST = ["WPL COMPUTER" , "T2" , "T3" , "t4"]
+x = input("PLease enter your password")
+if x == "password":
+    print("Welcome to the Database")
+    y = input("Who would you like to find? Press 1 if you want to list all users.")
+    if y == "WPL COMPUTER":
+        print("IP 209.212.23.47 Computer name waynead\461libref")
+    if y == "1":
+        print (LIST)
+        y = input("Who would you like to find? Press 1 if you want to list all users.")
+        if y == "WPL COMPUTER":
+            print("IP 209.212.23.47 Computer name waynead\461libref")
+    if y == "1":
+        print (LIST)
+       
+else:
+    print("Access Denied, please try again")
+    x = input("PLease re-enter your password")
+    if x == "2cr56zgj":
+        print("Welcome to the Database")
+        y = input("Who would you like to find? Press 1 if you want to list all users.")
+    else:
+        print ("The DataBase Is Now Locked")
+        security = input("WOULD YOU LIKE TO USE EMERGENCY DECRYPT KEY")
+        if security == "yes" and "Yes":
+            Emergancy_decript_key = "1234"
+            Emergancy_decript_key == input("Please enter the description code")
+            if Emergancy_decript_key == "1234":
+                print ("Welcome to the Database")
+print ("Contact me to add things to the DataBase or if bugs or logical errors occur")
